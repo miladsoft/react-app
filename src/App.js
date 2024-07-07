56742ed5cbf6f1e3575e9f2a4a889da714252d20
Milad Raeisi <miladsoft@yahoo.com>
2024-07-07 20:07:13 +0330

    Upload app

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <a
+          className="App-link"
+          href="https://miladraeisi.com"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Milad Raeisi
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
